6d3796e89dc6041596b5210a4594ead8d1c20932
Ovidijus <ovidijus.e@gmail.com>
2025-07-13 19:21:29 +0300

    Update cors config

--- backend/src/main/java/org/example/backend/config/CorsConfig.java
+++ backend/src/main/java/org/example/backend/config/CorsConfig.java
@@ -18,7 +18,11 @@ public class CorsConfig {
         config.setAllowedOrigins(List.of("http://localhost:4200",
                 "https://event-sync-gold.vercel.app",
                 "https://eventsync-production.up.railway.app",
-                "https://editor.swagger.io"));
+                "https://editor.swagger.io",
+                "https://petstore.swagger.io",
+                "https://app.swaggerhub.com",
+                "https://swagger.io"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
