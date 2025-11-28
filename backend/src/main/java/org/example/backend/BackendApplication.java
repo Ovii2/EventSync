18f6466b1f6df76f7c4def94288646591fbdf5e9
Ovidijus <ovidijus.e@gmail.com>
2025-11-28 16:44:16 +0200

    update security config setup

--- backend/src/main/java/org/example/backend/BackendApplication.java
+++ backend/src/main/java/org/example/backend/BackendApplication.java
@@ -5,12 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableAsync
-@EnableWebSecurity
-@EnableMethodSecurity
 @EnableScheduling
 @SpringBootApplication
 public class BackendApplication {
